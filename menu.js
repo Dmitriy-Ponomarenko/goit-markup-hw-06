c70c4e3ec37f9768ff4c857743cd1277d01b83a7
Dmitriy-Ponomarenko <dponomar19@gmail.com>
2024-11-08 21:16:35 +0100

    Add js for modal window

---
+++ menu.js
@@ -0,0 +1,42 @@
+// menu.js
+
+const modal = document.querySelector('.backdrop');
+const contactsLink = document.getElementById('contacts-link');
+const mobileContactsLink = document.getElementById('mobile-contacts-link');
+const mobileMenu = document.querySelector('.mobile-menu-wrapper');
+const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
+const mobileMenuCloseBtn = document.querySelector('.mobile-menu-close-btn');
+const modalCloseBtn = document.querySelector('.modal-close-btn');
+
+function openModal() {
+  modal.classList.add('is-open');
+}
+
+function closeModal() {
+  modal.classList.remove('is-open');
+}
+
+function openMobileMenu() {
+  mobileMenu.classList.add('is-open');
+}
+
+function closeMobileMenu() {
+  mobileMenu.classList.remove('is-open');
+}
+
+contactsLink.addEventListener('click', openModal);
+mobileContactsLink.addEventListener('click', () => {
+  closeMobileMenu();
+  openModal();
+});
+
+modalCloseBtn.addEventListener('click', closeModal);
+
+mobileMenuBtn.addEventListener('click', openMobileMenu);
+mobileMenuCloseBtn.addEventListener('click', closeMobileMenu);
+
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
